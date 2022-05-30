0f99bbc4cfb06c4da71d227af8e7420c3939e7e0
Alexandra Carla Atelli <101141804+alexandraatelli@users.noreply.github.com>
2022-05-30 18:16:50 -0300

    Atualização

--- exerciciosPOO_cs/ex004 Disciplina_cs/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿/*Crie uma classe chamada Disciplina que contenha os atributos nome da 
- * disciplina e carga horária. Tanto o nome da disciplina quanto a carga
- * horária são definidos pelo construtor da própria classe. Além do 
- * ,construtor, crie um método para exibir esses dados após a sua atribuição.
- * O método para exibir os dados deverá ser chamado em uma outra classe
- * */
