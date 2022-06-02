dd0e96d55780707a321c8f914aa1c35cb195b096
Alexandra Carla Atelli <101141804+alexandraatelli@users.noreply.github.com>
2022-06-02 16:08:00 -0300

    Atualização

--- exercicios_cs/WindowsForm/ProjetoWindonsForm1/Form1.Designer.cs
+++ exercicios_cs/WindowsForm/ProjetoWindonsForm1/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbCadastrar = new System.Windows.Forms.Button();
-            this.tbCancelar = new System.Windows.Forms.Button();
-            this.lblMsn = new System.Windows.Forms.Button();
+            this.TbCadastrar = new System.Windows.Forms.Button();
+            this.TbCancelar = new System.Windows.Forms.Button();
+            this.LblMsn = new System.Windows.Forms.Button();
             this.lbnome = new System.Windows.Forms.Label();
             this.lbemal = new System.Windows.Forms.Label();
             this.lbFone = new System.Windows.Forms.Label();
@@ -51,181 +51,202 @@
             this.tbNome = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // tbCadastrar
+            // TbCadastrar
             // 
-            this.tbCadastrar.Location = new System.Drawing.Point(170, 341);
-            this.tbCadastrar.Name = "tbCadastrar";
-            this.tbCadastrar.Size = new System.Drawing.Size(183, 45);
-            this.tbCadastrar.TabIndex = 0;
-            this.tbCadastrar.Text = "Cadastrar";
-            this.tbCadastrar.UseVisualStyleBackColor = true;
+            this.TbCadastrar.Location = new System.Drawing.Point(227, 420);
+            this.TbCadastrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TbCadastrar.Name = "TbCadastrar";
+            this.TbCadastrar.Size = new System.Drawing.Size(244, 55);
+            this.TbCadastrar.TabIndex = 0;
+            this.TbCadastrar.Text = "Cadastrar";
+            this.TbCadastrar.UseVisualStyleBackColor = true;
             // 
-            // tbCancelar
+            // TbCancelar
             // 
-            this.tbCancelar.Location = new System.Drawing.Point(464, 343);
-            this.tbCancelar.Name = "tbCancelar";
-            this.tbCancelar.Size = new System.Drawing.Size(183, 45);
-            this.tbCancelar.TabIndex = 1;
-            this.tbCancelar.Text = "Cancelar";
-            this.tbCancelar.UseVisualStyleBackColor = true;
+            this.TbCancelar.Location = new System.Drawing.Point(619, 422);
+            this.TbCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TbCancelar.Name = "TbCancelar";
+            this.TbCancelar.Size = new System.Drawing.Size(244, 55);
+            this.TbCancelar.TabIndex = 1;
+            this.TbCancelar.Text = "Cancelar";
+            this.TbCancelar.UseVisualStyleBackColor = true;
             // 
-            // lblMsn
+            // LblMsn
             // 
-            this.lblMsn.Location = new System.Drawing.Point(329, 406);
-            this.lblMsn.Name = "lblMsn";
-            this.lblMsn.Size = new System.Drawing.Size(163, 31);
-            this.lblMsn.TabIndex = 2;
-            this.lblMsn.UseVisualStyleBackColor = true;
+            this.LblMsn.Location = new System.Drawing.Point(439, 500);
+            this.LblMsn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LblMsn.Name = "LblMsn";
+            this.LblMsn.Size = new System.Drawing.Size(217, 38);
+            this.LblMsn.TabIndex = 2;
+            this.LblMsn.UseVisualStyleBackColor = true;
             // 
             // lbnome
             // 
             this.lbnome.AutoSize = true;
-            this.lbnome.Location = new System.Drawing.Point(30, 19);
+            this.lbnome.Location = new System.Drawing.Point(40, 23);
+            this.lbnome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbnome.Name = "lbnome";
-            this.lbnome.Size = new System.Drawing.Size(38, 13);
+            this.lbnome.Size = new System.Drawing.Size(47, 16);
             this.lbnome.TabIndex = 3;
             this.lbnome.Text = "Nome:";
             // 
             // lbemal
             // 
             this.lbemal.AutoSize = true;
-            this.lbemal.Location = new System.Drawing.Point(30, 45);
+            this.lbemal.Location = new System.Drawing.Point(40, 55);
+            this.lbemal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbemal.Name = "lbemal";
-            this.lbemal.Size = new System.Drawing.Size(37, 13);
+            this.lbemal.Size = new System.Drawing.Size(47, 16);
             this.lbemal.TabIndex = 4;
             this.lbemal.Text = "e-mail:";
             // 
             // lbFone
             // 
             this.lbFone.AutoSize = true;
-            this.lbFone.Location = new System.Drawing.Point(30, 72);
+            this.lbFone.Location = new System.Drawing.Point(40, 89);
+            this.lbFone.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbFone.Name = "lbFone";
-            this.lbFone.Size = new System.Drawing.Size(52, 13);
+            this.lbFone.Size = new System.Drawing.Size(64, 16);
             this.lbFone.TabIndex = 5;
             this.lbFone.Text = "Telefone:";
             // 
             // lbEndereco
             // 
             this.lbEndereco.AutoSize = true;
-            this.lbEndereco.Location = new System.Drawing.Point(30, 99);
+            this.lbEndereco.Location = new System.Drawing.Point(40, 122);
+            this.lbEndereco.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbEndereco.Name = "lbEndereco";
-            this.lbEndereco.Size = new System.Drawing.Size(56, 13);
+            this.lbEndereco.Size = new System.Drawing.Size(69, 16);
             this.lbEndereco.TabIndex = 6;
             this.lbEndereco.Text = "Endereço:";
             // 
             // lbBairro
             // 
             this.lbBairro.AutoSize = true;
-            this.lbBairro.Location = new System.Drawing.Point(31, 125);
+            this.lbBairro.Location = new System.Drawing.Point(41, 154);
+            this.lbBairro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbBairro.Name = "lbBairro";
-            this.lbBairro.Size = new System.Drawing.Size(37, 13);
+            this.lbBairro.Size = new System.Drawing.Size(46, 16);
             this.lbBairro.TabIndex = 7;
             this.lbBairro.Text = "Bairro:";
             // 
             // lbCidade
             // 
             this.lbCidade.AutoSize = true;
-            this.lbCidade.Location = new System.Drawing.Point(30, 151);
+            this.lbCidade.Location = new System.Drawing.Point(40, 186);
+            this.lbCidade.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbCidade.Name = "lbCidade";
-            this.lbCidade.Size = new System.Drawing.Size(43, 13);
+            this.lbCidade.Size = new System.Drawing.Size(54, 16);
             this.lbCidade.TabIndex = 8;
             this.lbCidade.Text = "Cidade:";
             // 
             // lbEstado
             // 
             this.lbEstado.AutoSize = true;
-            this.lbEstado.Location = new System.Drawing.Point(31, 178);
+            this.lbEstado.Location = new System.Drawing.Point(41, 219);
+            this.lbEstado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbEstado.Name = "lbEstado";
-            this.lbEstado.Size = new System.Drawing.Size(43, 13);
+            this.lbEstado.Size = new System.Drawing.Size(53, 16);
             this.lbEstado.TabIndex = 9;
             this.lbEstado.Text = "Estado:";
             // 
             // lbCep
             // 
             this.lbCep.AutoSize = true;
-            this.lbCep.Location = new System.Drawing.Point(31, 207);
+            this.lbCep.Location = new System.Drawing.Point(41, 255);
+            this.lbCep.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbCep.Name = "lbCep";
-            this.lbCep.Size = new System.Drawing.Size(31, 13);
+            this.lbCep.Size = new System.Drawing.Size(37, 16);
             this.lbCep.TabIndex = 10;
             this.lbCep.Text = "CEP:";
             // 
             // tbmail
             // 
-            this.tbmail.Location = new System.Drawing.Point(93, 38);
+            this.tbmail.Location = new System.Drawing.Point(124, 47);
+            this.tbmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbmail.Name = "tbmail";
-            this.tbmail.Size = new System.Drawing.Size(468, 20);
+            this.tbmail.Size = new System.Drawing.Size(623, 22);
             this.tbmail.TabIndex = 13;
             // 
             // tbFone
             // 
-            this.tbFone.Location = new System.Drawing.Point(93, 65);
+            this.tbFone.Location = new System.Drawing.Point(124, 80);
+            this.tbFone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbFone.Name = "tbFone";
-            this.tbFone.Size = new System.Drawing.Size(468, 20);
+            this.tbFone.Size = new System.Drawing.Size(623, 22);
             this.tbFone.TabIndex = 14;
             // 
             // tbEnd
             // 
-            this.tbEnd.Location = new System.Drawing.Point(93, 92);
+            this.tbEnd.Location = new System.Drawing.Point(124, 113);
+            this.tbEnd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbEnd.Name = "tbEnd";
-            this.tbEnd.Size = new System.Drawing.Size(468, 20);
+            this.tbEnd.Size = new System.Drawing.Size(623, 22);
             this.tbEnd.TabIndex = 15;
             // 
             // tbBairro
             // 
-            this.tbBairro.Location = new System.Drawing.Point(93, 118);
+            this.tbBairro.Location = new System.Drawing.Point(124, 145);
+            this.tbBairro.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbBairro.Name = "tbBairro";
-            this.tbBairro.Size = new System.Drawing.Size(468, 20);
+            this.tbBairro.Size = new System.Drawing.Size(623, 22);
             this.tbBairro.TabIndex = 16;
             // 
             // tbCidade
             // 
-            this.tbCidade.Location = new System.Drawing.Point(93, 144);
+            this.tbCidade.Location = new System.Drawing.Point(124, 177);
+            this.tbCidade.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbCidade.Name = "tbCidade";
-            this.tbCidade.Size = new System.Drawing.Size(468, 20);
+            this.tbCidade.Size = new System.Drawing.Size(623, 22);
             this.tbCidade.TabIndex = 17;
             // 
             // tbEstado
             // 
-            this.tbEstado.Location = new System.Drawing.Point(93, 171);
+            this.tbEstado.Location = new System.Drawing.Point(124, 210);
+            this.tbEstado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbEstado.Name = "tbEstado";
-            this.tbEstado.Size = new System.Drawing.Size(468, 20);
+            this.tbEstado.Size = new System.Drawing.Size(623, 22);
             this.tbEstado.TabIndex = 18;
             // 
             // tbCep
             // 
-            this.tbCep.Location = new System.Drawing.Point(93, 197);
+            this.tbCep.Location = new System.Drawing.Point(124, 242);
+            this.tbCep.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbCep.Name = "tbCep";
-            this.tbCep.Size = new System.Drawing.Size(468, 20);
+            this.tbCep.Size = new System.Drawing.Size(623, 22);
             this.tbCep.TabIndex = 19;
             // 
             // lbPais
             // 
             this.lbPais.AutoSize = true;
-            this.lbPais.Location = new System.Drawing.Point(31, 230);
+            this.lbPais.Location = new System.Drawing.Point(41, 283);
+            this.lbPais.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbPais.Name = "lbPais";
-            this.lbPais.Size = new System.Drawing.Size(32, 13);
+            this.lbPais.Size = new System.Drawing.Size(37, 16);
             this.lbPais.TabIndex = 20;
             this.lbPais.Text = "País:";
             // 
             // tbPais
             // 
-            this.tbPais.Location = new System.Drawing.Point(93, 223);
+            this.tbPais.Location = new System.Drawing.Point(124, 274);
+            this.tbPais.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbPais.Name = "tbPais";
-            this.tbPais.Size = new System.Drawing.Size(468, 20);
+            this.tbPais.Size = new System.Drawing.Size(623, 22);
             this.tbPais.TabIndex = 21;
             // 
             // tbNome
             // 
-            this.tbNome.Location = new System.Drawing.Point(93, 12);
+            this.tbNome.Location = new System.Drawing.Point(124, 15);
+            this.tbNome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbNome.Name = "tbNome";
-            this.tbNome.Size = new System.Drawing.Size(468, 20);
+            this.tbNome.Size = new System.Drawing.Size(623, 22);
             this.tbNome.TabIndex = 12;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.tbPais);
             this.Controls.Add(this.lbPais);
             this.Controls.Add(this.tbCep);
@@ -244,9 +265,10 @@
             this.Controls.Add(this.lbFone);
             this.Controls.Add(this.lbemal);
             this.Controls.Add(this.lbnome);
-            this.Controls.Add(this.lblMsn);
-            this.Controls.Add(this.tbCancelar);
-            this.Controls.Add(this.tbCadastrar);
+            this.Controls.Add(this.LblMsn);
+            this.Controls.Add(this.TbCancelar);
+            this.Controls.Add(this.TbCadastrar);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Formulario";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -257,9 +279,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button tbCadastrar;
-        private System.Windows.Forms.Button tbCancelar;
-        private System.Windows.Forms.Button lblMsn;
+        private System.Windows.Forms.Button TbCadastrar;
+        private System.Windows.Forms.Button TbCancelar;
+        private System.Windows.Forms.Button LblMsn;
         private System.Windows.Forms.Label lbnome;
         private System.Windows.Forms.Label lbemal;
         private System.Windows.Forms.Label lbFone;
--- exercicios_cs/WindowsForm/ProjetoWindonsForm1/Form1.cs
+++ exercicios_cs/WindowsForm/ProjetoWindonsForm1/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Programa em processo de criação - Início 01.06.2022
+
 namespace ProjetoWindonsForm1
 {
     public partial class Form1 : Form
@@ -21,24 +23,32 @@ namespace ProjetoWindonsForm1
         {
             MessageBox.Show("Bem vindo! Faça o seu Cadastro.");
         }
-        private void tbCadastrar_Click(object sender, EventArgs e)
+        private void TbCadastrar_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Preenchido os dados, click em Cadastrar");
             MessageBox.Show($"Nome: {tbNome.Text}\n" +
                $"e-mail: {tbmail.Text}\n" +
-               $"Endereço: {tbEnd.Text}\n Bairro: {tbBairro.Text}\n" +
-               $"Cidade: {tbCidade.Text}\n" +
-               $"Telefone: {tbFone.Text}");
+               $"Telefone: {tbFone.Text}\n" +
+               $"Endereço: {tbEnd.Text}\n " +
+               $"Bairro: {tbBairro.Text}\n" +
+               $"Cidade: {tbCidade.Text}\n " +
+               $"CEP: {tbCep.Text}\n" +
+               $"Estado:{tbEstado.Text}\n" +
+               $"País: {tbPais.Text}");
             MessageBox.Show("Cadastro efetuado!");
             tbNome.Clear();
             tbmail.Clear();
+            tbFone.Clear();
             tbEnd.Clear();
-            tbBairro.Text = "";
-            tbCidade.Text = "Nova Cidade";
-            tbFone.Text = "";
-            lblMsn.Text = "CADASTRO EFETUADO COM SUCESSO! INSIRA UM NOVO REGISTRO!";
+            tbBairro.Clear();
+            tbCidade.Clear();
+            tbEstado.Clear();
+            tbCep.Clear();
+            tbPais.Clear();
+            //tbPais.Text = "";
+            LblMsn.Text = "Cadastro efetuado com sucesso! Insira outro registro, se necessário.";
         }
-        private void tbCancelar_Click(object sender, EventArgs e)
+        private void TbCancelar_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Clique em Cancelar para excluir seu cadastro.");
         }
