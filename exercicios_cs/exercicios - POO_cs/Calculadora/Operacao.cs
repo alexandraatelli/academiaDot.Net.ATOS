7abad73d1cc31fb0a5e72aba712a59924a094843
Alexandra Carla Atelli <101141804+alexandraatelli@users.noreply.github.com>
2022-06-03 18:09:51 -0300

    Atualização

---
+++ exercicios_cs/exercicios - POO_cs/Calculadora/Operacao.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.cs
+{
+    public class Operacao
+    {
+        /// <summary>
+        /// Atributos. São variáveis;
+        /// </summary>
+        double n1;
+        double n2;
+
+        /// <summary>
+        /// Construtor - eu o chamo, no programa principal quando instanciar.
+        /// Atalho para criar método construtor = ctor
+        /// </summary>
+        public Operacao()  
+        {
+          
+        }
+
+        public Operacao(double n1, double n2)
+        {
+            this.N1 = n1; // O this é usado no N maiusculo. Porque nas variaveis internas geram o get e set
+            this.N2 = n2;
+        }
+        /// <summary>
+        /// Métodos para efetuar as operações matemáticas. E fica dentro da classe calculadora pois aqui é o lugar dele.
+        /// </summary>
+        public double Somar(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public double Subtrair(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public double Dividir(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+        public double Multiplicar(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        // Marca n1 e n2, click mouse direito, e cria-se o Get e Set automaticamente
+        public double N1 { get => n1; set => n1 = value; }
+        public double N2 { get => n2; set => n2 = value; }
+    }
+}
---
+++ exercicios_cs/exercicios - POO_cs/Calculadora/Program.cs
@@ -0,0 +1,29 @@
+﻿using Calculadora.cs;
+
+///<summary>
+/// instanciação do objeto - utilizando "new"
+/// </summary>
+Operacao operacao = new Operacao();
+
+double resultado;
+resultado = operacao.Somar(2, 3); //Passando parametro, e chamar o método da classe, retornado um resultado
+Console.WriteLine(resultado);
+
+///<summary>
+///instanciando outro objeto e utilizando os métodos da classe Operacao
+/// </summary>
+Operacao operacao1 = new Operacao(10, 20);
+Console.WriteLine("Atributos da Classe (Objeto operacao1: " + operacao1.N1 + " " + operacao1.N2);
+
+resultado = operacao1.Somar(2, 3);
+Console.WriteLine(resultado);
+
+resultado = operacao1.Subtrair(2, 3);
+Console.WriteLine(resultado);
+
+resultado = operacao1.Dividir(2, 3);
+Console.WriteLine(resultado);
+
+resultado = operacao1.Multiplicar(2, 3);
+Console.WriteLine(resultado);
+
