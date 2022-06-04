76093ea4394c05ec8754d8dc2ba704bde90ad58c
Alexandra Carla Atelli <101141804+alexandraatelli@users.noreply.github.com>
2022-06-03 22:38:20 -0300

    Atualização

---
+++ exercicios_cs/WindowsForm/WindowsFormsApp2/Carro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    class Carro
+    {
+        public string ModeloCarro { get; set; }
+        public string FabricanteCarro { get; set; }
+        
+        public bool arCond;
+        public bool dirHidr;
+        public bool abs;
+        public bool airBg;
+        public bool vidElet;
+        public int portas;
+        public Carro(string modeloCarro, string fabricanteCarro, bool arCond, bool dirHidr, bool abs, bool airBg, bool vidlet, int portas)
+        {   // é a variável da classe = o Modelo da Classe é igual o modelo daqui.
+            this.ModeloCarro = modeloCarro;
+            this.FabricanteCarro = fabricanteCarro;
+            this.arCond = arCond;
+            this.dirHidr = dirHidr;
+            this.abs = abs;
+            this.airBg = airBg;
+            this.vidElet = vidlet;
+            this.portas = portas;
+        }
+
+        public void MostrarDadosCarro()
+        {
+            MessageBox.Show($"Carro: {this.ModeloCarro}");
+            MessageBox.Show($"Fabricante: {this.ModeloCarro}");
+            MessageBox.Show($"Ar Condicionado: {this.ModeloCarro}");
+            MessageBox.Show($"Direção Hidráulica: {this.dirHidr}");
+            MessageBox.Show($"ABS: {this.abs}");
+            MessageBox.Show($"Airbag: {this.airBg}");
+            MessageBox.Show($"Vidros Elétricos: {this.vidElet}");
+            MessageBox.Show($"Portas: {this.portas}");
+        }
+    }
+}
---
+++ exercicios_cs/WindowsForm/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void tbLimpar_Click(object sender, EventArgs e)
+        {
+            FabricCarro.Clear();
+            ModeloCarro.Clear();
+
+            foreach (int checados in lbOpcionais.CheckedItems)
+            {
+                lbOpcionais.SetItemChecked(checados, false);
+            }
+            cbQuantPortas.ResetText();
+        }
+
+        private void tbSalvar_Click(object sender, EventArgs e)//Método inicia letra Maiúscula - Variável do método.
+        {
+            bool arCond = false, dirHidr = false, abs = false, airBg = false, vidElet = false;
+            int portas;
+
+            foreach (string opcionais in lbOpcionais.CheckedItems)
+            {
+                if (opcionais == "Ar Condicionado")
+                {
+                    arCond = true;
+                }
+                if (opcionais == "Direção Hidráulica")
+                {
+                    dirHidr = true;
+                }
+                if (opcionais == "Freio ABS")
+                {
+                    abs = true;
+                }
+                if (opcionais == "Air bag")
+                {
+                    airBg = true;
+                }
+                if (opcionais == "Air bag")
+                {
+                    vidElet = true;
+                }
+            }
+            portas = 0;
+            if (cbQuantPortas.Text == "2 Portas") portas = 2;
+            else if (cbQuantPortas.Text == "4 Portas") portas = 4;
+            else if (cbQuantPortas.Text == "6 Portas") portas = 6;
+            Carro car = new Carro(ModeloCarro.Text, FabricCarro.Text, arCond, dirHidr, abs, airBg, vidElet, portas);
+            car.MostrarDadosCarro();
+        }
+    }
+}
+
