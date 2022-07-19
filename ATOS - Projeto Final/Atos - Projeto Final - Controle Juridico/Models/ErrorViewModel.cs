a78754ca1c4f9bdbe449063bbe0f73fc64f83e12
Alexandra Carla Atelli <101141804+alexandraatelli@users.noreply.github.com>
2022-07-19 16:21:42 -0300

    Atualização - ATOS Projeto Final

--- ATOS - Projeto Final/Atos - Projeto Final - Controle Juridico/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AppControleJuridico.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
